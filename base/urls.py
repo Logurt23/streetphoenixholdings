134eb038b1a2f5b4f680e3287b025a7a4adb037b
Logan Brewer <logan@fbstudios.com>
2020-11-29 21:10:42 -0600

    static conversion

--- base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import homepage, events
-
-# base App urls
-
-urlpatterns = [
-    path('', homepage, name='homepage'),
-    path('events/', events, name='events'),
-]
